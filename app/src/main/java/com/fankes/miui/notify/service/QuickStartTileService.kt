ea953d6bcffeadb657a0da6514818724b3b07761
Fankesyooni <qzmmcn@163.com>
2022-03-26 12:26:00 +0800

    增加通知栏快捷磁贴打开在线规则列表功能

---
+++ app/src/main/java/com/fankes/miui/notify/service/QuickStartTileService.kt
@@ -0,0 +1,36 @@
+/*
+ * MIUINativeNotifyIcon - Fix the native notification bar icon function abandoned by the MIUI development team.
+ * Copyright (C) 2019-2022 Fankes Studio(qzmmcn@163.com)
+ * https://github.com/fankes/MIUINativeNotifyIcon
+ *
+ * This software is non-free but opensource software: you can redistribute it
+ * and/or modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ * <p>
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * and eula along with this software.  If not, see
+ * <https://www.gnu.org/licenses/>
+ *
+ * This file is Created by fankes on 2022/3/26.
+ */
+package com.fankes.miui.notify.service
+
+import android.service.quicksettings.TileService
+import com.fankes.miui.notify.ui.activity.ConfigureActivity
+import com.fankes.miui.notify.utils.factory.navigate
+
+class QuickStartTileService : TileService() {
+
+    override fun onClick() {
+        super.onClick()
+        /** 启动通知图标优化列表窗口 */
+        navigate<ConfigureActivity>()
+    }
+}
