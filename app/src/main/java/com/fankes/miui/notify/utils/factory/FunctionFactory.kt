fa373f8c2de9afc18e0f9547c1167ca04204862a
fankesyooni <qzmmcn@163.com>
2023-09-18 00:21:12 +0800

    feat: simple support Android 14 display name

--- app/src/main/java/com/fankes/miui/notify/utils/factory/FunctionFactory.kt
+++ app/src/main/java/com/fankes/miui/notify/utils/factory/FunctionFactory.kt
@@ -158,6 +158,7 @@ inline val isNotSupportMiuiVersion get() = !isSupportMiuiVersion
  */
 val androidVersionCodeName
     get() = when (Build.VERSION.SDK_INT) {
+        34 -> "U"
         33 -> "T"
         32 -> "S_V2"
         31 -> "S"
@@ -171,7 +172,7 @@ val androidVersionCodeName
         23 -> "M"
         22 -> "L_MR1"
         21 -> "L"
-        else -> ""
+        else -> "?"
     }
 
 /**
