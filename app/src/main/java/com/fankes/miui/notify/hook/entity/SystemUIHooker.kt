dab9181ca0b917deb315bffe6bf509800fec247f
YuSaki丶Kanade <53483352+Nep-Timeline@users.noreply.github.com>
2024-12-07 01:03:48 +0800

    opt: do not make unnecessary changes

--- app/src/main/java/com/fankes/miui/notify/hook/entity/SystemUIHooker.kt
+++ app/src/main/java/com/fankes/miui/notify/hook/entity/SystemUIHooker.kt
@@ -93,6 +93,7 @@ import com.highcapable.yukihookapi.hook.type.android.ImageViewClass
 import com.highcapable.yukihookapi.hook.type.android.NotificationClass
 import com.highcapable.yukihookapi.hook.type.android.RemoteViewsClass
 import com.highcapable.yukihookapi.hook.type.android.StatusBarNotificationClass
+import com.highcapable.yukihookapi.hook.type.java.BooleanClass
 import com.highcapable.yukihookapi.hook.type.java.BooleanType
 import com.highcapable.yukihookapi.hook.type.java.IntType
 import top.defaults.drawabletoolbox.DrawableBuilder
@@ -892,8 +893,8 @@ object SystemUIHooker : YukiBaseHooker() {
              * 强制修改 getCustomAppIcon 获取的图标为 smallIcon
              * 部分系统没有 "getCustomAppIcon" 这个方法 - 所以直接忽略
              */
-            if (hasMethod { name = "getCustomAppIcon" })
-                method {
+            if (hasMethod { name = "getCustomAppIcon" }){
+                method{
                     name = "getCustomAppIcon"
                     param(NotificationClass, ContextClass)
                 }.hook().after {
@@ -901,15 +902,19 @@ object SystemUIHooker : YukiBaseHooker() {
                     val ct = args(1).cast<Context>()
                     val smail = nf?.smallIcon?.loadDrawable(ct)?.toBitmap()
                     if (smail != null && !smail.isRecycled) {
-                        if (ct != null)
-                            result = BitmapDrawable(ct.resources, smail)
-                    } else result = null
+                        if (ct != null) {
+                            result =  BitmapDrawable(ct.resources, smail)
+                        }
+                    } else {
+                        result = null
+                    }
                 }
+            }
             /**
              * 强制回写系统的状态栏图标样式为原生
              * 部分系统没有 "getSmallIcon" 这个方法 - 所以直接忽略
              */
-            if (hasMethod { name = "getSmallIcon" })
+            if (hasMethod { name = "getSmallIcon" }){
                 method {
                     name = "getSmallIcon"
                     param { it[0] extends StatusBarNotificationClass && it[1] == IntType }
@@ -933,6 +938,7 @@ object SystemUIHooker : YukiBaseHooker() {
                         ).also { pair -> if (pair.second) result = Icon.createWithBitmap(pair.first?.toBitmap()) }
                     }
                 }
+            }
         }
         /** 去他妈的焦点通知彩色图标 */
         FocusUtils?.apply {
@@ -1193,7 +1199,7 @@ object SystemUIHooker : YukiBaseHooker() {
             /**
              * MIUI 14 ([RemoteViewsClass], [NotificationClass])
              * HyperOS ([RemoteViewsClass], [NotificationClass], [ContextClass])
-             * HyperOS 2.0 ([RemoteViewsClass], [NotificationClass], [ContextClass], [Boolean])
+             * HyperOS 2.0 ([RemoteViewsClass], [NotificationClass], [ContextClass], [BooleanClass])
              */
             paramCount(numRange = 2..4)
         }?.hook()?.intercept()
