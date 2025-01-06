5ae8d09c285da57a8df7f8c6a94c9d20eefb3cdd
ghhccghk <2137610394@qq.com>
2025-01-06 20:10:52 +0800

    fix: 修复调用方式解决Hyper 1.0 焦点反色失败

--- app/src/main/java/com/fankes/miui/notify/hook/entity/SystemUIHooker.kt
+++ app/src/main/java/com/fankes/miui/notify/hook/entity/SystemUIHooker.kt
@@ -979,7 +979,7 @@ object SystemUIHooker : YukiBaseHooker() {
                     YLog.debug("FocusedNotifPromptView DEBUG $isDark ${mIcon.any()}")
                 /** HyperOS 1.x 旧版本适配 */
                 if (miosVersion == "1.0" || miosVersion == "1.1")
-                    mIcon.current()?.superClass()!!.method { name = "setColorFilter" }.call(if (isDark <= 0.5f) Color.WHITE else Color.BLACK)
+                    mIcon.current()?.superClass()?.method { name = "setColorFilter" }?.call(if (isDark <= 0.5f) Color.WHITE else Color.BLACK)
             }
         }
         /** 去他妈的焦点通知彩色图标 */
