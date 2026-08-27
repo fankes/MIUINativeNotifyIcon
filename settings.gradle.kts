5105121ac4aa2f466b46512c4827a1aebe0c37a4
fankesyooni <qzmmcn@163.com>
2026-08-27 21:10:22 +0800

    chore: update configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -26,12 +26,12 @@ gropify {
                 "^project\\..*$".toRegex()
             )
             permanentKeyValues("GITHUB_CI_COMMIT_ID" to "")
+            // 手动指定类型，防止一些特殊 "COMMIT ID" 被生成为数值
+            keyValuesRules("GITHUB_CI_COMMIT_ID" to ValueRule(String::class))
             locations(GropifyLocation.RootProject, GropifyLocation.SystemEnv)
         }
         android {
             includeKeys("GITHUB_CI_COMMIT_ID")
-            // 手动指定类型，防止一些特殊 "COMMIT ID" 被生成为数值
-            keyValuesRules("GITHUB_CI_COMMIT_ID" to ValueRule(String::class))
         }
     }
     rootProject { common { isEnabled = false } }
